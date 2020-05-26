a4474648f4c8c653ff3c20dcdba7d17f65ebc895
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-25 21:36:55 -0300

    Model e controler de User para criação no dbCHE

---
+++ src/controllers/UserController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+
+
+module.exports = {
+    async store(req, res){
+        try {
+            const {firstName, lastName, password, email} = req.body;
+
+            const user = await User.create({
+                firstName: firstName,
+                lastName: lastName,
+                password: password,
+                email: email
+            });
+
+            return res.json(user);
+
+        } catch (error) {
+            throw Error(`Error while registering a nre user: ${error}`);
+        }
+    }
+}
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const UserController = require('../src/controllers/UserController');
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello from Nodemon')
 });
+
+app.post('/register', UserController.store);
 
 try {
     mongoose.connect(process.env.MONGO_DB_CONNECTION, {
