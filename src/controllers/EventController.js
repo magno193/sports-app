e6b3303becc8f1abc13ea36223df5166005950e4
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-26 17:01:21 -0300

    Event completo

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -5,10 +5,10 @@ const User = require('../models/User');
 module.exports = {
     async createEvent(req, res) {
         const {title, description, price} = req.body;
-        const {userId} = req.headers;
+        const {user_id} = req.headers;
         const {filename} = req.file;
 
-        const user = await User.findById(userId);
+        const user = await User.findById(user_id);
 
         if(!user){
             return res.status(400).json({ message: 'User does not exist!' })
@@ -17,8 +17,8 @@ module.exports = {
         const event = await Event.create({
             title,
             description,
-            price,
-            user: userId,
+            price: parseFloat(price),
+            user: user_id,
             thumbnail: filename
         })
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const routes = express.Router();
+const multer = require('multer');
+
 const UserController = require('../src/controllers/UserController');
+const EventController = require('./controllers/EventController');
+const uploadConfig = require('./config/upload');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.get('/status', (req, res) => {
     res.send({ status: 200 });
 });
 
+
+// Event
+routes.post('/event', upload.single("thumbnail"), EventController.createEvent)
+
+// User
 routes.post('/user/register', UserController.createUser);
 routes.get('/user/:userId', UserController.getUserById);
 
