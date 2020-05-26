42203a305d4c732ffcc4555c29a3114cc3ea0a61
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-26 15:44:24 -0300

    Model e controller de Event - inicio

---
+++ src/controllers/EventController.js
@@ -0,0 +1,28 @@
+const Event = require('../models/Event');
+const User = require('../models/User');
+
+
+module.exports = {
+    async createEvent(req, res) {
+        const {title, description, price} = req.body;
+        const {userId} = req.headers;
+        const {filename} = req.file;
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(400).json({ message: 'User does not exist!' })
+        }   
+
+        const event = await Event.create({
+            title,
+            description,
+            price,
+            user: userId,
+            thumbnail: filename
+        })
+
+        return res.json(event);
+
+    }
+}
