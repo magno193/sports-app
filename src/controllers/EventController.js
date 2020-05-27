36e407721900f15b8bc9383f806b373c07a174e4
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-27 20:01:47 -0300

    Abstração de métodos de event para dashboard

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -27,34 +27,6 @@ module.exports = {
 
     },
 
-    async getEventById(req, res) {
-
-        const {eventId} = req.params;
-        try {
-            const event = await Event.findById(eventId)
-
-            if (event) {
-                return res.json(event)
-            }
-        } catch (error) {
-            return res.status(400).json({ message: 'Event does not exist!'});        }
-    },
-
-    async getAllEvents(req, res) {
-        const {sport} = req.params;
-        const query = { sport } || {};
-        try {
-            const events = await Event.find(query);
-
-            if(events) {
-                return res.json(events);
-            }
-
-        } catch (error) {
-            return res.status(400).json({ message: `We don't have any events yet` });
-        }
-    },
-
     async delete(req, res) {
         const { eventId } = req.params;
 
