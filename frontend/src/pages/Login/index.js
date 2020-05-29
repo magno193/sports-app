f2a3dcbc1d00160e7a923ba24db83a83eeed240a
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-28 22:28:50 -0300

    Usando useState no formulário de login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import api from '../../services/api';
 import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
 
 export default function Login() {
+
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleSubmit = async event => {
+        event.preventDefault(); // cancela refresh
+    }
+
     return(
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                <Input type='email' name='email' id='exampleEmail' placeholder='Your e-mail'></Input>
+                <Input type='email' name='email' id='exampleEmail' placeholder='Your e-mail' onChange={event => setEmail(event.target.value)} />
             </FormGroup>
             <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                <Input type='password' name='password' id='examplePassword' placeholder='Your password'></Input>
+                <Input type='password' name='password' id='examplePassword' placeholder='Your password' onChange={event => setPassword(event.target.value)}/>
             </FormGroup>
             <Button>Submit</Button>
         </Form>
