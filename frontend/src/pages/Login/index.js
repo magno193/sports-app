0fb79c604d500f20696a40abfeec0aa198fd2d90
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-28 23:08:23 -0300

    Componente Registration

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -24,10 +24,10 @@ export default function Login({history}) {
     return(
         <Form onSubmit={handleSubmit}>
             <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                <Input type='email' name='email' id='exampleEmail' placeholder='Your e-mail' onChange={event => setEmail(event.target.value)} />
+                <Input type='email' name='email' id='email' placeholder='Your e-mail' onChange={event => setEmail(event.target.value)} />
             </FormGroup>
             <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                <Input type='password' name='password' id='examplePassword' placeholder='Your password' onChange={event => setPassword(event.target.value)}/>
+                <Input type='password' name='password' id='password' placeholder='Your password' onChange={event => setPassword(event.target.value)}/>
             </FormGroup>
             <Button>Submit</Button>
         </Form>
---
+++ frontend/src/pages/Registration/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import api from "../../services/api";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+export default function Registration({ history }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // cancela refresh
+
+    const response = await api.post("/login", { email, password });
+    const userId = response.data._id || false;
+
+    if (userId) {
+      localStorage.setItem("user", userId);
+      history.push("/login");
+    } else {
+      const { message } = response.data;
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Input
+          type="text"
+          name="firstName"
+          id="firstName"
+          placeholder="Your first name"
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Input
+          type="text"
+          name="lastName"
+          id="lastName"
+          placeholder="Your last name"
+          onChange={(event) => setLastName(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your e-mail"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Your password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+  );
+}
