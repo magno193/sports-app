b1256d1ba106a036616cdbb0b1be900d89747733
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-28 22:20:46 -0300

    Página Lógin com reactstrap

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import api from '../../services/api';
+import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
 
 export default function Login() {
     return(
-        <div>
-            Hello from Login
-        </div>
-    )
+        <Form>
+            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                <Input type='email' name='email' id='exampleEmail' placeholder='Your e-mail'></Input>
+            </FormGroup>
+            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                <Input type='password' name='password' id='examplePassword' placeholder='Your password'></Input>
+            </FormGroup>
+            <Button>Submit</Button>
+        </Form>
+    );
 }
