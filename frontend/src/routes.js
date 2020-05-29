83805457721d96a7726c7a2c8577ab3820cef38e
Alexandre Ferreira <alexmlf93@gmail.com>
2020-05-28 23:11:18 -0300

    Rota para o componente Registration

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login/';
 import Dashboard from './pages/Dashboard/';
+import Registration from './pages/Registration/';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Login} />
+                <Route path='/registration' exact component={Registration} />
                 <Route path='/dashboard' component={Dashboard} />
             </Switch>
         </BrowserRouter>
